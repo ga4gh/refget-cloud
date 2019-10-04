238201f94ce56051297b65a07d92ebe14447fd6e
Jeremy Adams <jeremy.adams@ga4gh.org>
2019-10-04 13:57:37 -0400

    added unittests, improved test coverage

---
+++ refget-app/unittests/test_refget/test_middleware/test_media_type.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""Unit tests for the media type midware
+"""
+
+import json
+from config.constants import *
+from middleware.start import StartMidware
+from middleware.media_type import MediaTypeMW, MediaTypeMidware
+
+def test_media_type_midware():
+
+    test_cases = [
+        # INVALID MEDIA TYPE
+        {
+            "event": {
+                "headers": {
+                    "Accept": "text/strange1, text/strange2;"
+                }
+            },
+            "context": {},
+            "assertions": {
+                "statusCode": 406,
+                "headers": {},
+                "body": json.dumps({
+                    "message": "requested media type(s) not supported"
+                })
+            }
+        },
+        # VALID MEDIA TYPE: */*
+        {
+            "event": {
+                "headers": {
+                    "Accept": "*/*;"
+                }
+            },
+            "context": {},
+            "assertions": {
+                "statusCode": 200,
+                "headers": {
+                    "Content-Type": DEFAULT_CONTENT_TYPE_JSON
+                },
+                "body": ""
+            }
+        }
+    ]
+
+    for case in test_cases:
+
+        @StartMidware(case["event"], case["context"])
+        @MediaTypeMidware(case["event"], case["context"])
+        def worker(resp):
+            return resp
+        r = worker().finalize()
+        assert case["assertions"]["statusCode"] == r["statusCode"]
+        assert case["assertions"]["body"] == r["body"]
+        for hkey in case["assertions"]["headers"].keys():
+            assert case["assertions"]["headers"][hkey] == r["headers"][hkey]
